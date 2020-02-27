c76bf4b791d9417514eb2aabf03fa8abc86338b3
Cristian Alejandro Ramirez Holguin <cristian.ramirez3@ibm.com>
2020-02-27 13:48:02 -0500

    Carga de archivos

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3200;
+const www = process.env.WWW || './';
+
+app.use(express.static(__dirname + '/dist/angularp1'));
+
+console.log(`serving ${www}`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname));
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`listening on http://localhost:${port}`));
